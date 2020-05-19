0a72d8c90c314e3ef33b31995aa9acee1c3af572
NastyaQueenZhenya <queen@MacBook-Pro-Silkina.local>
2020-05-19 13:45:39 +0300

    updated file from web

---
+++ task_1.js
@@ -0,0 +1,16 @@
+
+function convertToF(celsius) {
+    var a = 9/5;
+    var c = 32;
+    var fahrenheit = (a * celsius) + c
+    console.log(fahrenheit);
+    return fahrenheit;
+  }
+  
+  convertToF(30);
+  convertToF(0);
+  convertToF(30);
+  convertToF(-10);
+  convertToF(0);
+  convertToF(20);
+  convertToF(30);
